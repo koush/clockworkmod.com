4d6c583d6d5400b392f56f766d8bd58fc5994c15
Koushik Dutta <koushd@gmail.com>
2016-06-01 19:44:12 -0700

    wtf

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 // var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -15,7 +15,7 @@ app.set('view engine', 'jade');
 // app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/carbon', function(req, res) {
